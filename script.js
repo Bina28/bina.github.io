9a3cf015b07dc97f8cf1da98274c77d95b070c67
258068 <129079992+258068@users.noreply.github.com>
2024-02-20 02:04:12 +0100

    Slideshow
    That i spent 10 hours on <3

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //#region account-dropdown
+
+
 var accountDropdown = document.getElementById("account-dropdown"); // Get the "My account" dropdown item
 var dropdownContent = accountDropdown.querySelector(".dropdown-content"); // Get the dropdown content
 
@@ -10,13 +12,68 @@ accountDropdown.addEventListener("click", function(event) {
     dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block"; // Toggle the display of the dropdown content
 });
 
-// Hide the dropdown menu when clicking outside of it
+// Get rid of dropdown menu when clicking outside of it
 document.addEventListener("click", function(event) {
     if (!dropdownContent.contains(event.target) && event.target !== accountDropdown) {
         dropdownContent.style.display = "none";
     }
 });
+
+// Lead to their respective pages when clicked on elements inside "My account"
+document.addEventListener("DOMContentLoaded", function() {
+    const dropdownItems = document.querySelectorAll(".dropdown-item a");
+
+    dropdownItems.forEach(item => {
+        item.addEventListener("click", function(event) {
+            event.preventDefault();
+            const url = this.getAttribute("href");
+            window.location.href = url;
+        });
+    });
+});
+
+// Perform logout functionality and redirect the user back to the main page, e.g. index.html
+document.addEventListener("DOMContentLoaded", function() {
+    const logoutLink = document.getElementById("logout");
+
+    logoutLink.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        window.location.href = "index.html";
+    });
+});
+
+
 //#endregion account-dropdown
 
+//#region slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
 
+// next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("drinkSlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
+
+//#endregion slideshow
